7881d0624c247798390685ee578b2d5ef864bc3c
48878679 <152203542+48878679@users.noreply.github.com>
2023-11-29 11:23:50 -0600

    Update PlaylistNode.cpp
    Generated playlistNode

--- PlaylistNode.cpp
+++ PlaylistNode.cpp
@@ -1,1 +1,34 @@
-/* Type your code here */
+#include "PlaylistNode.h"
+#include <iostream>
+
+PlaylistNode::PlaylistNode() : uniqueID("none"), songName("none"), artistName("none")
+PlaylistNode::PlaylistNode(std::string id, std::string name, std::string artist, int length) : uniqueID(id), songName(name), artistName(artist), songLength(length), nextNodePtr(nullptr){}
+std::string PlaylistNode::GetID() const{
+  return uniqueID;
+}
+std::string PlaylistNode::GetSongName() const {
+  return songName;
+}
+std::string PlatlistNode::GetArtistName() const{
+  return artistName;
+}
+int PlaylistNode::GetSongLength() const{
+  return songLength;
+}
+PlaylistNode* PlaylistNode::GetNext() const{
+  return nextNodePtr;
+}
+void PlaylistNode::InsertAfter(PlaylistNode* nodePtr){
+  PlaylistNode* temp = nextNodePtr;
+  nextNodePtr = nodePtr;
+  nodePtr->nextNodePtr = temp;
+}
+void PlaylistNode::SetNext(PlaylistNode* nodePtr){
+  nextNodePtr = nodePtr;
+}
+void PlaylistNode::PrintPlaylistNode() const{
+  std::cout << "Unique ID: " << uniqueID << std::endl;
+  std::cout << "Song Name: " << songName << std::endl;
+  std::cout << "Artist Name: " << artistName << std::endl;
+  std::cout << "Song Length (int seconds): " << songLength << std::endl;
+}
