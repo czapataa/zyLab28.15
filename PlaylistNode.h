2aecb6dbe7bae68d5ad96f9cea6b186d0e840f11
czapataa <152098790+czapataa@users.noreply.github.com>
2023-11-29 11:22:35 -0600

    Update PlaylistNode.h

--- PlaylistNode.h
+++ PlaylistNode.h
@@ -1,1 +1,29 @@
-/* Type your code here */
+#ifndef PLAYLISTNODE_H
+#define PLAYLISTNODE_H
+
+#include <string>
+
+class PlaylistNode{
+private:
+  std::string uniqueID;
+  std::string songName;
+  std::string artistName;
+  int songLength;
+  PlaylistNode* nextNodePtr;
+
+public:
+  PlaylistNode();
+  PlaylistNode(std::string id, std::string songName, std::string artistName, int songLength);
+
+  std::string GetID() const;
+  std::string GetSongName() const;
+  std::steing GetArtistName() const;
+  int GetSongLength() const;
+  PlaylistNode* GetNext() const;
+
+  void InsertAfter(PlaylistNode* nodePtr);
+  void SetNext(PlaylistNode* nodePtr);
+  void PrintPlaylistNode() const;
+};
+
+#endif
