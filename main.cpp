b732ca4f7eba380409f91bca674b13cdb7542fa1
czapataa <152098790+czapataa@users.noreply.github.com>
2023-11-29 11:31:14 -0600

    Update main.cpp
    main()

--- main.cpp
+++ main.cpp
@@ -14,7 +14,21 @@ PlaylistNode* ExecuteMenu(char option, string playlistTitle, PlaylistNode* headN
 }
 
 int main() {
-   /* Type your code here */
-   
+   std::string playlistTitle;
+   char option;
+   PlaylistNode* headNode = nullptr;
+   cout <<"Enter playlist's title:" << endl;
+   cout << endl;
+   getline(cin, playlistTitle);
+
+   do{
+      PrintMenu(playlistTitle);
+      cout <<"Choose an option:" << endl;
+      cin >> option;
+      if(cin.peek() == '\n){
+      cin.ignore();
+      }
+      headNode = ExecuteMenu(option,playlistTitle, headNode);
+   } while(option != 'q');
    return 0;
 }
